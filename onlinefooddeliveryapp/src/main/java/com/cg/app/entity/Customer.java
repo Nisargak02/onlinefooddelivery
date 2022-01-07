b0c6b86770702d4eb9df8cdefafeeef4e793a2b8
KandraPreethi <kandrapreethi@gmail.com>
2022-01-07 19:42:00 +0530

    Added Address class in com.cg.app.entity package Modified application.properties file Modified build.gradle file Modified Customer.java file Modified main class file

--- onlinefooddeliveryapp/src/main/java/com/cg/app/entity/Customer.java
+++ onlinefooddeliveryapp/src/main/java/com/cg/app/entity/Customer.java
@@ -1,14 +1,22 @@
 package com.cg.app.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
 	private String firstName;
 	private String lastName;
 	private Integer age;
 	private String gender;
 	private String mobileNumber;
-	private String address;
+	private Address address;
 	private String email;
 	
 	
