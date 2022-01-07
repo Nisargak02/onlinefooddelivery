889a7ce1ca5a99c77dc4df298d49b2b02ad67abc
KandraPreethi <kandrapreethi@gmail.com>
2022-01-07 11:28:51 +0530

    Added 3 packages and classes Made some changes in application properties, build.gradle and in main class.

--- onlinefooddeliveryapp/src/main/java/com/cg/app/OnlinefooddeliveryappApplication.java
+++ onlinefooddeliveryapp/src/main/java/com/cg/app/OnlinefooddeliveryappApplication.java
@@ -1,9 +1,10 @@
-package com.example.demo;
+package com.cg.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@
 public class OnlinefooddeliveryappApplication {
 
 	public static void main(String[] args) {
---
+++ onlinefooddeliveryapp/src/main/java/com/cg/app/entity/Customer.java
@@ -0,0 +1,16 @@
+package com.cg.app.entity;
+
+public class Customer {
+	
+	private Integer customerId;
+	private String firstName;
+	private String lastName;
+	private Integer age;
+	private String gender;
+	private String mobileNumber;
+	private String address;
+	private String email;
+	
+	
+
+}
